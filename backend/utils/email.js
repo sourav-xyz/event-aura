28fd39250cb131680db33d33d4810f7c3744dc41
SOURABHA BISOYI <sourabhabisoyi18@gmail.com>
2026-05-05 04:04:38 +0530

    Update email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -5,7 +5,7 @@ const createTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
